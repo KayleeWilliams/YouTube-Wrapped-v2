b99622d04a94a68406450ad1b4748984649952d0
Kaylee Williams <65376239+KayleeWilliams@users.noreply.github.com>
2022-09-20 20:36:49 +0100

    Setup Next.js, set up Tailwind CSS & Unplugin-Icons Completed some of the frontend.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#1C1B1D',
+        link: '#D282A6',
+        on: '#F58D9B',
+      },
+      // fontFamily: {
+      //   'custom': ['Roboto', 'Open Sans', 'Arial'],
+      // },
+    },
+  },
+  plugins: [],
+}
